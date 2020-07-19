e172f592891c9d540ad742fcfca346fa2cfac6eb
Felipe Barbalho <felipebarbalho.95@gmail.com>
2020-07-19 12:27:21 -0300

    Updates

---
+++ precache-manifest.326ed89e2733c2308acba54db8b67d1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e77d1a95cb52d3282ca0ab688a4edca",
+    "url": "/todolist-reactjs/index.html"
+  },
+  {
+    "revision": "b3a7ffa806184a9af559",
+    "url": "/todolist-reactjs/static/js/2.7b72a845.chunk.js"
+  },
+  {
+    "revision": "141d9ba221855f45693be48945b4fa04",
+    "url": "/todolist-reactjs/static/js/2.7b72a845.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "633956465fb0bbf3b165",
+    "url": "/todolist-reactjs/static/js/main.10a32b69.chunk.js"
+  },
+  {
+    "revision": "70116277ed219cdfed3c",
+    "url": "/todolist-reactjs/static/js/runtime-main.383dc0fd.js"
+  }
+]);
